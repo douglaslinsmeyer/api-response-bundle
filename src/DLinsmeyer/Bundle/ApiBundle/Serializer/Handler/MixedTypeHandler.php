50e258a704f4109ca16540aa2083271b6949e6c7
Daniel Lakes <dlakes@nerdery.com>
2014-09-04 15:27:47 -0500

    Updated serialization to pass type with Mixed data

--- src/DLinsmeyer/Bundle/ApiBundle/Serializer/Handler/MixedTypeHandler.php
+++ src/DLinsmeyer/Bundle/ApiBundle/Serializer/Handler/MixedTypeHandler.php
@@ -55,16 +55,6 @@ class MixedTypeHandler implements SubscribingHandlerInterface
         );
     }
 
-    /**
-     * Examines the actual type of $value and serializes based on that type
-     *
-     * @param JsonSerializationVisitor $visitor
-     * @param $value
-     * @param array $type
-     * @param SerializationContext $context
-     *
-     * @return mixed
-     */
     public function serializeMixedTypeToJson(JsonSerializationVisitor $visitor, $value, array $type, SerializationContext $context)
     {
         $isObject = is_object($value);
@@ -75,18 +65,38 @@ class MixedTypeHandler implements SubscribingHandlerInterface
             'params' => array()
         );
 
+        /**
+         * We need to store the type information along w/ the object on
+         * serialization. To do this, we're taking advantage of
+         * the array<k,T> type, with k being our class/type expressed as a
+         * string, and T being our data
+         */
+        $wrapperArr = array(
+            'name' => 'array',
+            'params' => array(
+                array(
+                    'name' => 'string',
+                    'params' => array(),
+                ),
+                $typeArr
+            )
+        );
+        $wrappedData = array(
+            $dataType => $value
+        );
+
         if($isObject){
-            /*
+            /**
              * this has to be done to ensure the serializer will properly parse this value
              * See JMS/Serializer/GraphNavigator.php line 143 (if ($context->isVisiting($data)) [...])
              */
             $context->stopVisiting($value);
-            $result = $visitor->getNavigator()->accept($value, $typeArr, $context);
+            $result = $visitor->getNavigator()->accept($wrappedData, $wrapperArr, $context);
             //now that we're done, reattach so it can handle all of its cleanup
             $context->startVisiting($value);
             return $result;
         } else {
-            return $visitor->getNavigator()->accept($value, $typeArr, $context);
+            return $visitor->getNavigator()->accept($value, $wrapperArr, $context);
         }
     }
 
